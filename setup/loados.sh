0c6cc0f123c19bc91462edc9d9bf02902d2fa449
mrAlexZT <mr.AlexZT@gmail.com>
2016-12-22 17:48:12 +0200

    Update loados.sh

--- setup/loados.sh
+++ setup/loados.sh
@@ -7,6 +7,12 @@ yum install -y epel-release remi-release
 
 yum update -y && yum upgrade -y
 
-yum install -y mc git wget tar htop bash-completion vsftpd net-tools tcpdump telnet supervisor make cmake gcc gcc-* libstdc++-devel compat-libstdc++-33.i686 libstdc++.i686 libstdc++-devel.i686 glibc-devel.i686
+yum install -y fio
 
-systemctl enable vsftpd
+#dd if=/dev/urandom of=/tmp/sample.txt bs=16G count=1
+
+fio --name=randwrite --ioengine=libaio --iodepth=30 --rw=randwrite --bs=4k --direct=1 --size=16G --numjobs=16 --runtime=604800 --group_reporting
+
+fio --name=randread --ioengine=libaio --iodepth=30 --rw=randread --bs=4k --direct=1 --size=16G --numjobs=16 --runtime=604800 --group_reporting
+
+
