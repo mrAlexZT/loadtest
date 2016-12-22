4c4177450a37ccd499b1fb221afae63f583283ee
mrAlexZT <mr.AlexZT@gmail.com>
2016-12-22 17:54:34 +0200

    Update loados.sh

--- setup/loados.sh
+++ setup/loados.sh
@@ -11,8 +11,8 @@ yum install -y fio
 
 #dd if=/dev/urandom of=/tmp/sample.txt bs=16G count=1
 
-fio --name=randwrite --ioengine=libaio --iodepth=30 --rw=randwrite --bs=4k --direct=1 --size=16G --numjobs=16 --runtime=604800 --group_reporting
+#fio --name=randwrite --ioengine=libaio --iodepth=30 --rw=randwrite --bs=4k --direct=1 --size=16G --numjobs=16 --runtime=604800 --group_reporting
 
-fio --name=randread --ioengine=libaio --iodepth=30 --rw=randread --bs=4k --direct=1 --size=16G --numjobs=16 --runtime=604800 --group_reporting
+#fio --name=randread --ioengine=libaio --iodepth=30 --rw=randread --bs=4k --direct=1 --size=16G --numjobs=16 --runtime=604800 --group_reporting
 
 exit 0
